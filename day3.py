a917144adb5afc369eb42a9a9c04015460283152
cevolaniconnor <70539453+cevolaniconnor@users.noreply.github.com>
2024-12-11 16:01:05 -0800

    Day 3 pt.1

---
+++ day3.py
@@ -0,0 +1,26 @@
+import re 
+
+file = "day3puzzle.txt"
+word = r'mul\(\d+,\d+\)'
+total = 0
+muls = []
+
+with open(file, 'r') as f:
+	content = f.read()
+
+matches = re.findall(word, content)
+
+for match in matches:
+	numbers = re.findall(r'\d+', match)
+	for i in range(len(numbers)):
+		# Converts each element to an integer
+		numbers[i] = int(numbers[i])
+
+		mult = numbers[0] * numbers[1]
+
+	total += mult
+
+print(total)
+
+
+f.close()
